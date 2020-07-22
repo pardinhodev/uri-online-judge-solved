6015c7fb0913f86cc275401685e5fd493a7af268
pardinhodev <allanspdr@gmail.com>
2020-07-21 22:31:34 -0300

    update_uri_1001

---
+++ uri1001/uri1001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uri1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, r;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            r = a + b;
+            Console.WriteLine("X = " + r);
+        }
+    }
+}
