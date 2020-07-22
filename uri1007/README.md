fb3083492e0e1fb5e6e944d531c8afdc6d64bddf
pardinhodev <allanspdr@gmail.com>
2020-07-22 11:40:21 -0300

    uri1007

---
+++ uri1007/README.md
@@ -0,0 +1,20 @@
+# Difference
+
+**Adapted by Neilor Tonin, URI - Brazil**
+
+**Timelimit: 1**
+#
+
+**Problem:**
+
+Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+
+- **Input:**
+
+The input file contains 4 integer values.
+
+- **Output:**
+
+Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters, according to the following example, with a blank space before and after the equal signal.
+
+- **Example:**
---
+++ uri1007/uri1007/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uri1007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, r1, r2, rf;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+
+            r1 = a * b;
+            r2 = c * d;
+            rf = r1 - r2;
+
+            Console.WriteLine("DIFERENCA = " + rf);
+        }
+    }
+}
