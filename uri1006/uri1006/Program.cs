51877abe254f9576f3ab4c644b8e1cf5cdf4b669
pardinhodev <allanspdr@gmail.com>
2020-07-22 11:35:31 -0300

    uri1006

---
+++ uri1006/uri1006/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c, media;
+
+            a = Convert.ToSingle(Console.ReadLine());
+            b = Convert.ToSingle(Console.ReadLine());
+            c = Convert.ToSingle(Console.ReadLine());
+            media = (c * 5 + (b * 3 + (a * 2))) / 10;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1"));
+        }
+    }
+}
