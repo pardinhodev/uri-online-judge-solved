86a6bfacb5fcf2f5c3421d091c5110e786972a8a
pardinhodev <allanspdr@gmail.com>
2020-07-22 11:09:16 -0300

    uri1003

---
+++ uri1003/uri1003/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, s;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            s = a + b;
+
+            Console.WriteLine("SOMA = " + s);
+        }
+    }
+}
