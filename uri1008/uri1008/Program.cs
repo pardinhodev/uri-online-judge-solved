eda0bcc56852e74ed230395c318a18a26d66669b
pardinhodev <allanspdr@gmail.com>
2020-07-22 11:46:26 -0300

    uri1008

---
+++ uri1008/uri1008/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uri1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, h;
+            double s, r;
+
+            n = int.Parse(Console.ReadLine());
+            h = int.Parse(Console.ReadLine());
+            s = double.Parse(Console.ReadLine());
+            r = h * s;
+
+            Console.WriteLine("NUMBER = " + n);
+            Console.WriteLine("SALARY = U$ " + r.ToString("F2"));
+        }
+    }
+}
