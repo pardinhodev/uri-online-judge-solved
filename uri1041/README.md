2c357c289be2a0927f31bd9d62c9eac6c8396164
pardinhodev <allanspdr@gmail.com>
2020-07-22 16:23:23 -0300

    uri1041

---
+++ uri1041/README.md
@@ -0,0 +1,24 @@
+# Coordinates of a Point
+
+**Adapted by Neilor Tonin, URI - Brazil**
+
+**Timelimit: 1**
+#
+
+**Problem:**
+
+Write an algorithm that reads two floating values (x and y), which should represent the coordinates of a point in a plane. Next, determine which quadrant the point belongs, or if you are over one of the Cartesian axes or the origin (x = y = 0).
+
+If the point is at the origin, write the message "Origem".
+
+If the point is over X axis write "Eixo X", else if the point is over Y axis write "Eixo Y".
+
+- **Input:**
+
+The input contains the coordinates of a point.
+
+- **Output:**
+
+The output should display the quadrant in which the point is.
+
+- **Example:**
---
+++ uri1041/uri1041/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1041
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y;
+            string[] coordinates = Console.ReadLine().Split(' ');
+            x = float.Parse(coordinates[0], CultureInfo.InvariantCulture);
+            y = float.Parse(coordinates[1], CultureInfo.InvariantCulture);
+
+            if (x > 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0.0 && y < 0.0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else if (x > 0.0 && y < 0.0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else if (x == 0.0 && y == 0.0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (x > 0.0 || x < 0.0 && y == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else
+            {
+                Console.WriteLine("Eixo Y");
+            }
+        }
+    }
+}
