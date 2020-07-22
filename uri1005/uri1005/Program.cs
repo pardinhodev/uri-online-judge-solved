f94689c0b2886899a8073d24efb66fe5da54f46d
pardinhodev <allanspdr@gmail.com>
2020-07-22 11:30:55 -0300

    uri1005

---
+++ uri1005/uri1005/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1, p2, media;
+
+            p1 = Convert.ToDouble(Console.ReadLine());
+            p2 = Convert.ToDouble(Console.ReadLine());
+            media = (p2 * 7.5 + (p1 * 3.5)) / 11;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F5"));
+        }
+    }
+}
