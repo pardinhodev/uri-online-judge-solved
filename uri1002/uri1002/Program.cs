6872322f9706962ff849d3741b0ce450667c1ab5
pardinhodev <allanspdr@gmail.com>
2020-07-22 10:57:05 -0300

    uri1002

---
+++ uri1002/uri1002/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, r, a;
+
+            n = 3.14159;
+            r = Convert.ToDouble(Console.ReadLine());
+            a = n * Math.Pow(r, 2);
+
+            Console.WriteLine("A=" + a.ToString("F4"));
+        }
+    }
+}
