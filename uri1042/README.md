95322896a29326e9f9d4eb848f15912bd9733530
pardinhodev <allanspdr@gmail.com>
2020-07-22 22:05:03 -0300

    uri1042

---
+++ uri1042/README.md
@@ -0,0 +1,20 @@
+# Simple Sort
+
+**Adapted by Neilor Tonin, URI - Brazil**
+
+**Timelimit: 1**
+#
+
+**Problem:**
+
+Read three integers and sort them in ascending order. After, print these values in ascending order, a blank line and then the values in the sequence as they were readed.
+
+- **Input:**
+
+The input contains three integer numbers.
+
+- **Output:**
+
+Present the output as requested above.
+
+- **Example:**
---
+++ uri1042/uri1042/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace uri1042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne, numberTwo, numberThree;
+            string[] numbers = Console.ReadLine().Split(' ');
+
+            numberOne = int.Parse(numbers[0]);
+            numberTwo = int.Parse(numbers[1]);
+            numberThree = int.Parse(numbers[2]);
+
+            if (numberOne > numberTwo && numberOne > numberThree && numberTwo > numberThree)
+            {
+                Console.WriteLine(numberThree + "\n" + numberTwo + "\n" + numberOne);
+            }
+            else if (numberOne > numberTwo && numberOne > numberThree && numberThree > numberTwo)
+            {
+                Console.WriteLine(numberTwo + "\n" + numberThree + "\n" + numberOne);
+            }
+            else if (numberTwo > numberOne && numberTwo > numberThree && numberOne > numberThree)
+            {
+                Console.WriteLine(numberThree + "\n" + numberOne + "\n" + numberTwo);
+            }
+            else if (numberTwo > numberOne && numberTwo > numberThree && numberThree > numberOne)
+            {
+                Console.WriteLine(numberOne + "\n" + numberThree + "\n" + numberTwo);
+            }
+            else if (numberThree > numberOne && numberThree > numberTwo && numberOne > numberTwo)
+            {
+                Console.WriteLine(numberTwo + "\n" + numberOne + "\n" + numberThree);
+            }
+            else
+            {
+                Console.WriteLine(numberOne + "\n" + numberTwo + "\n" + numberThree);
+            }
+
+            Console.WriteLine("\n" + numberOne);
+            Console.WriteLine(numberTwo);
+            Console.WriteLine(numberThree);
+
+        }
+    }
+}
