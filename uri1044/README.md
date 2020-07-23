b0c014c3175546e76d345bb7829f6e157051e32a
pardinhodev <allanspdr@gmail.com>
2020-07-22 23:33:02 -0300

    uri1044

---
+++ uri1044/README.md
@@ -0,0 +1,20 @@
+# Multiples
+
+**Adapted by Neilor Tonin, URI - Brazil**
+
+**Timelimit: 1**
+#
+
+**Problem:**
+
+Read two nteger values (A and B). After, the program should print the message "Sao Multiplos" (are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values.
+
+- **Input:**
+
+The input has two integer numbers.
+
+- **Output:**
+
+Print the relative message to the input as stated above.
+
+- **Example:**
---
+++ uri1044/uri1044/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uri1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            string[] numbers = Console.ReadLine().Split(' ');
+
+            a = int.Parse(numbers[0]);
+            b = int.Parse(numbers[1]);
+
+            if (a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
